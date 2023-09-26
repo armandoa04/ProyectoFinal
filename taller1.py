3e981f5599e6bfd7643be59accf43b0ee1b91912
Armando Acosta <armandoa04@hotmail.com>
2023-09-26 17:08:11 -0500

    esta es una monficacion de los valores

--- taller1.py
+++ taller1.py
@@ -1,8 +1,8 @@
-num1=int(input("ingrese primer valor:"))
-num2=int(input("ingrese segundo valor:"))
+num1=int(input("ingrese primer numero:"))
+num2=int(input("ingrese segundo numero:"))
 suma=num1+num2
 producto=num1*num2
-print("La suma de los dos valores es")
+print("La suma de los dos numeros es")
 print(suma)
-print("El producto de los dos valores es")
+print("El producto de los dos numeros es")
 print(producto)   
