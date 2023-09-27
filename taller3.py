7da1f1782ad98aed069662e2d800a40511408051
Armando Acosta <armandoa04@hotmail.com>
2023-09-27 11:53:41 -0500

    Agregando taller3

---
+++ taller3.py
@@ -0,0 +1,5 @@
+precio=int(input("Ingrese el precio del producto:"))
+cantidad=int(input("Ingrese la cantidad de productos a llevar:"))
+importe=precio*cantidad
+print("El total a pagar es")
+print(importe)
